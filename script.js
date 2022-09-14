d7fcf5960eb3076063f4ea0556c5b51ea3f7ae7e
Syeda Alam <syedahalam@yahoo.com>
2022-09-14 19:21:41 -0400

    js

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-let letters = ['this is it', 'hi buddy'];
-console.log(letters.reverse());
+const numbers = [
+	[1, 2, 3, 4],
+	[5, 6, 7, 8],
+	[9, 10, 11, 12],
+	[13, 14, 15, 16],
+];
+let total = 0;
+for (let i = 0; i < numbers.length; i++) {
+	let rows = numbers[i];
+	for (let j = 0; j < rows.length; j++) {
+		total += rows[j];
+	}
+}
+console.log(total);
