004c95d785353d20a231cbb8ee102bdeea350390
Syeda Alam <syedahalam@yahoo.com>
2022-09-15 09:54:56 -0400

    js

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-const numbers = [
-	[1, 2, 3, 4],
-	[5, 6, 7, 8],
-	[9, 10, 11, 12],
-	[13, 14, 15, 16],
-];
-let total = 0;
-for (let i = 0; i < numbers.length; i++) {
-	let rows = numbers[i];
-	for (let j = 0; j < rows.length; j++) {
-		total += rows[j];
-	}
-}
-console.log(total);
+const dice = (num) => console.log(Math.floor(Math.random() * 6 + 1));
+
+dice();
