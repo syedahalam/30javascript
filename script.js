85e29a88f2a05e48d6f63a0c220677806ffb1d73
Syeda Alam <syedahalam@yahoo.com>
2022-09-16 10:02:30 -0400

    js

--- script.js
+++ script.js
@@ -1,3 +1,54 @@
+// const pickACard = () => {
+// 	const rand1 = [
+// 		'2',
+// 		'3',
+// 		'4',
+// 		'5',
+// 		'6',
+// 		'7',
+// 		'8',
+// 		'9',
+// 		'10',
+// 		'J',
+// 		'Q',
+// 		'K',
+// 		'A',
+// 	];
+// 	const suites = ['clubs', 'diamonds', 'hearts', 'spades'];
+// 	rand(rand1);
+// 	rand(suites);
+// 	// const idx = Math.floor(Math.random() * rand1.length);
+// 	// const idx1 = Math.floor(Math.random() * suites.length);
+// 	return `{\n Value is: ${rand1[idx]}\n The Suite is: ${suites[idx1]} \n}`;
+// };
+// console.log(pickACard());
+
+// const rand = (aqq) => {
+// 	const idx = Math.floor(Math.random() * aqq.length);
+// 	return aqq[idx];
+// };
+
+// const random = () => {
+// 	const values = [
+// 		'2',
+// 		'3',
+// 		'4',
+// 		'5',
+// 		'6',
+// 		'7',
+// 		'8',
+// 		'9',
+// 		'10',
+// 		'J',
+// 		'Q',
+// 		'K',
+// 		'A',
+// 	];
+// 	const suites = ['Clubs', 'Diamonds', 'Hearts', 'Spades'];
+// 	return `{\n  Value: ${rand(values)}\n  Suite: ${rand(suites)}\n}`;
+// };
+// console.log(random());
+
 // const isPangram = (str) => {
 // 	for (let char of 'abcdefghijklmnopqrstuvwxyz') {
 // 		if (str.includes(char)) {
