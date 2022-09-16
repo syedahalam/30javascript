f67127226ccea91949b5fbc773cb6dfae3c8ad01
Syeda Alam <syedahalam@yahoo.com>
2022-09-16 09:30:22 -0400

    js

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
-const isValidPassword = (password, username) => {
-	if (password.length < 8 && password === username) {
-		return false;
-	}
-	if (password.indexOf(' ') !== -1) {
-		return false;
-	}
-	return true;
-};
-console.log(isValidPassword('syeda123', 'syeda'));
+// const isPangram = (str) => {
+// 	for (let char of 'abcdefghijklmnopqrstuvwxyz') {
+// 		if (str.includes(char)) {
+// 			return true;
+// 		}
+// 	}
+// 	return false;
+// };
+// console.log(isPangram('the five boxing wizards jump quickl'));
+
+// const average = (arr) => {
+// 	let total = 0;
+// 	for (let num of arr) {
+// 		total += num;
+// 	}
+// 	return total / arr.length;
+// };
+// console.log(average([75, 76, 80, 95, 100]));
+
+// const isValidPassword = (password, username) =>
+// 	!password.length < 8 ||
+// 	!password === username ||
+// 	password.indexOf(' ') !== -1;
+
+// // if (
+// // 	password.length < 8 ||
+// // 	password === username ||
+// // 	password.indexOf(' ') !== -1
+// // ) {
+// // 	return false;
+// // }
+
+// // return true;
+
+// console.log(isValidPassword('syeda123', 'syeda'));
